e17ce5ff8938296509315a612e5f74912fcd356f
David <mr.davidbauer@gmail.com>
2022-06-03 05:22:00 +0200

    + List benchmark modularized + cleaning, test results, etc.

--- MicrobenchmarkSample/microbenchmark/src/androidTest/java/com/example/benchmark/GetTopSportBenchmark.kt
+++ MicrobenchmarkSample/microbenchmark/src/androidTest/java/com/example/benchmark/GetTopSportBenchmark.kt
@@ -2,23 +2,71 @@ package com.example.benchmark
 
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.example.benchmark.data.*
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-//@SmallTest
-@RunWith(AndroidJUnit4::class)
+@SmallTest
 class GetTopSportBenchmark {
     @get:Rule
     val benchmarkRule = BenchmarkRule()
+
+    private val sportStats = listOf(
+        Summary(Sport.HIKE, 92),
+        Summary(Sport.RUN, 77),
+        Summary(Sport.TOURING_BICYCLE, 322),
+        Summary(Sport.E_TOURING_BICYCLE, 656),
+        Summary(Sport.HIKE, 91),
+        Summary(Sport.RUN, 76),
+        Summary(Sport.TOURING_BICYCLE, 321),
+        Summary(Sport.E_TOURING_BICYCLE, 655),
+        Summary(Sport.HIKE, 90),
+        Summary(Sport.RUN, 76),
+        Summary(Sport.TOURING_BICYCLE, 320),
+        Summary(Sport.E_TOURING_BICYCLE, 654),
+        Summary(Sport.HIKE, 89),
+        Summary(Sport.RUN, 75),
+        Summary(Sport.TOURING_BICYCLE, 319),
+        Summary(Sport.E_TOURING_BICYCLE, 653),
+        Summary(Sport.HIKE, 88),
+        Summary(Sport.RUN, 74),
+        Summary(Sport.TOURING_BICYCLE, 318),
+        Summary(Sport.E_TOURING_BICYCLE, 652),
+        Summary(Sport.HIKE, 87),
+        Summary(Sport.RUN, 73),
+        Summary(Sport.TOURING_BICYCLE, 317),
+        Summary(Sport.E_TOURING_BICYCLE, 651),
+        Summary(Sport.HIKE, 88),
+        Summary(Sport.RUN, 72),
+        Summary(Sport.TOURING_BICYCLE, 316),
+        Summary(Sport.E_TOURING_BICYCLE, 650),
+        Summary(Sport.HIKE, 89),
+        Summary(Sport.RUN, 71),
+        Summary(Sport.TOURING_BICYCLE, 315),
+        Summary(Sport.E_TOURING_BICYCLE, 649),
+    )
+
+    /*
+        With 4 items in list:
+        184 ns 1 allocs PROFILED_GetTopSportBenchmark.benchmarkGetTopSportManualIteration
+        185 ns 1 allocs PROFILED_GetTopSportBenchmark.benchmarkGetTopSportManual
+        366 ns 4 allocs PROFILED_GetTopSportBenchmark.benchmarkGetTopSportSequence
+        386 ns 4 allocs PROFILED_GetTopSportBenchmark.benchmarkGetTopSportAggregated
+     */
+
+    /*
+        With 32 items in list:
+        1,026 ns 1 allocs PROFILED_GetTopSportBenchmark.benchmarkGetTopSportManualIteration
+        996 ns 1 allocs PROFILED_GetTopSportBenchmark.benchmarkGetTopSportManual
+        1,627 ns 4 allocs PROFILED_GetTopSportBenchmark.benchmarkGetTopSportSequence
+        2,005 ns 4 allocs PROFILED_GetTopSportBenchmark.benchmarkGetTopSportAggregated
+    */
 
     @Test
     fun benchmarkGetTopSportAggregated() {
         benchmarkRule.measureRepeated {
             getTopSportAggregated(sportStats)
-//            printTopSport(getTopSportAggregated(sportStats))
         }
     }
 
@@ -26,7 +74,6 @@ class GetTopSportBenchmark {
     fun benchmarkGetTopSportSequence() {
         benchmarkRule.measureRepeated {
             getTopSportSequence(sportStats)
-//            printTopSport(getTopSportSequence(sportStats))
         }
     }
 
@@ -34,7 +81,6 @@ class GetTopSportBenchmark {
     fun benchmarkGetTopSportManual() {
         benchmarkRule.measureRepeated {
             getTopSportManual(sportStats)
-//            printTopSport(getTopSportManual(sportStats))
         }
     }
 
@@ -42,97 +88,6 @@ class GetTopSportBenchmark {
     fun benchmarkGetTopSportManualIteration() {
         benchmarkRule.measureRepeated {
             getTopSportManualIteration(sportStats)
-//            printTopSport(getTopSportManualIteration(sportStats))
         }
     }
-}
-
-enum class Sport { HIKE, RUN, TOURING_BICYCLE, E_TOURING_BICYCLE }
-
-data class Summary(val sport: Sport, val distance: Int)
-
-val sportStats = listOf(
-    Summary(Sport.HIKE, 92),
-    Summary(Sport.RUN, 77),
-    Summary(Sport.TOURING_BICYCLE, 322),
-    Summary(Sport.E_TOURING_BICYCLE, 656)
-)
-
-fun main() {
-
-    // Write kotlin code to print the top sport by distance excluding eBikes.
-
-    printTopSport(getTopSportAggregated(sportStats))
-    // Benchmark: 380-402ns and 4 alloc
-    // Problem: this call requires multiple iteration over the list.
-
-    // Alternative 1. Using sequence.
-    printTopSport(getTopSportSequence(sportStats))
-    // Benchmark: 358-369ns and 4 alloc
-    // Problem: most likely not worth the extra overhead?
-
-    // Alternative 2. Doing it manually, best performance.
-    printTopSport(getTopSportManual(sportStats))
-    // Benchmark: 181-186ns and 1 alloc.
-    // Problem: it supposes there would always be data in the list and -1 being the lowest value.
-
-    // Alternative 3. Iterating manually over list.
-    printTopSport(getTopSportManualIteration(sportStats))
-    // Benchmark: 185ns and 1 alloc
-    // Problem: more work, readability, etc.
-}
-
-private fun printTopSport(maxEntry: Summary?) {
-    println("top sport by distance: $maxEntry")
-}
-
-private fun getTopSportManual(
-    sportStats: List<Summary>
-): Summary? {
-    return sportStats.maxByOrNull {
-        when (it.sport.name) {
-            Sport.E_TOURING_BICYCLE.name -> -1
-            else -> it.distance
-        }
-    }
-}
-
-private fun getTopSportSequence(
-    sportStats: List<Summary>
-): Summary? {
-    return sportStats
-        .asSequence()
-        .filter { it.sport.name != Sport.E_TOURING_BICYCLE.name }
-        .maxByOrNull { it.distance }
-}
-
-private fun getTopSportAggregated(sportStats: List<Summary>): Summary? {
-    return sportStats
-        .filter { it.sport.name != Sport.E_TOURING_BICYCLE.name }
-        .maxByOrNull { it.distance }
-}
-
-private fun getTopSportManualIteration(sportStats: List<Summary>): Summary? {
-    val iterator = sportStats.iterator()
-    if (!iterator.hasNext()) return null
-    var maxElem = iterator.next()
-    if (!iterator.hasNext()) {
-        return if (maxElem.sport.name == Sport.E_TOURING_BICYCLE.name) {
-            null
-        } else {
-            maxElem
-        }
-    }
-    var maxValue = maxElem.distance
-    do {
-        val e = iterator.next()
-        if (e.sport.name != Sport.E_TOURING_BICYCLE.name) {
-            val v = e.distance
-            if (maxValue < v) {
-                maxElem = e
-                maxValue = v
-            }
-        }
-    } while (iterator.hasNext())
-    return maxElem
 }
